5530f32ef3f6349824de0f34adc31dfce7121625
Backul <23520088@gm.uit.edu.vn>
2026-05-17 11:39:11 +0700

    feat(supportNeed): add support need enums

---
+++ src/main/java/com/helphub/backend/common/enums/SupportNeedUnit.java
@@ -0,0 +1,13 @@
+package com.helphub.backend.common.enums;
+
+public enum SupportNeedUnit {
+    VND,
+    KG,
+    PIECE,
+    BOX,
+    LITER,
+    PACKAGE,
+    SET,
+    PERSON,
+    OTHER
+}
---
+++ src/main/java/com/helphub/backend/common/enums/SupportType.java
@@ -0,0 +1,6 @@
+package com.helphub.backend.common.enums;
+
+public enum SupportType {
+    MONEY,
+    GOODS
+}
