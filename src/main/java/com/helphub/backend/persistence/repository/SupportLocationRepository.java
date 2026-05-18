917de36552c882153e023fd62447c35857a638b4
Backul <23520088@gm.uit.edu.vn>
2026-05-18 20:22:30 +0700

    feat(support-location): add support location queries

--- src/main/java/com/helphub/backend/persistence/repository/SupportLocationRepository.java
+++ src/main/java/com/helphub/backend/persistence/repository/SupportLocationRepository.java
@@ -1,9 +1,17 @@
 package com.helphub.backend.persistence.repository;
 
 import com.helphub.backend.persistence.entity.SupportLocation;
+import com.helphub.backend.persistence.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface SupportLocationRepository extends JpaRepository<SupportLocation, UUID> {
+
+    List<SupportLocation> findAllByOrderByCreatedAtDesc();
+
+    List<SupportLocation> findAllByIsActiveTrueOrderByCreatedAtDesc();
+
+    List<SupportLocation> findAllByCreatedByOrderByCreatedAtDesc(User createdBy);
 }
