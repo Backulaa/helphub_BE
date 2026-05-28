22d6fecaa4bb30de79b9fa9b31f8ec86556f46e5
Backul <23520088@gm.uit.edu.vn>
2026-05-28 21:04:40 +0700

    feat(report): add report enums

---
+++ src/main/java/com/helphub/backend/common/enums/ReportStatus.java
@@ -0,0 +1,7 @@
+package com.helphub.backend.common.enums;
+
+public enum ReportStatus {
+    PENDING,
+    REVIEWED,
+    RESOLVED
+}
---
+++ src/main/java/com/helphub/backend/common/enums/ReportTargetType.java
@@ -0,0 +1,7 @@
+package com.helphub.backend.common.enums;
+
+public enum ReportTargetType {
+    SUPPORT_REQUEST,
+    POST,
+    USER
+}
